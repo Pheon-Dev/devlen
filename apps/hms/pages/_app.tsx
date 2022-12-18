56092f99d824327b3edefa76a762f22f24a4dbc8
Pheon-Dev <devpheon@gmail.com>
2022-12-18 11:42:19 +0300

    chore: appshell separation from landing page

--- apps/hms/pages/_app.tsx
+++ apps/hms/pages/_app.tsx
@@ -4,8 +4,27 @@ import { getCookie, setCookie } from 'cookies-next';
 import Head from 'next/head';
 import { MantineProvider, ColorScheme, ColorSchemeProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
+import {
+  AppShell,
+  Navbar,
+  Header,
+  Footer,
+  Aside,
+  Title,
+  Text,
+  MediaQuery,
+  Burger,
+  useMantineTheme,
+} from '@mantine/core';
+import useStyles from '../styles';
+import { MainLinks, Welcome } from '../components';
+import { useRouter } from 'next/router';
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
+  const { classes } = useStyles();
+  const theme = useMantineTheme();
+  const router = useRouter();
+  const [opened, setOpened] = useState(false);
   const { Component, pageProps } = props;
   const [colorScheme, setColorScheme] = useState<ColorScheme>(props.colorScheme);
 
@@ -26,7 +45,67 @@ export default function App(props: AppProps & { colorScheme: ColorScheme }) {
       <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
         <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
           <NotificationsProvider>
-            <Component {...pageProps} />
+          {router && router.pathname === "/" && <Welcome /> ||
+            <AppShell
+              /* styles={{ */
+              /*   main: { */
+              /*     background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0], */
+              /*   }, */
+              /* }} */
+              navbarOffsetBreakpoint="sm"
+              asideOffsetBreakpoint="sm"
+              navbar={
+                <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
+                  <Navbar.Section grow mt="xs">
+                    <MainLinks />
+                  </Navbar.Section>
+                  {/* <Navbar.Section> */}
+                  {/*   <User /> */}
+                  {/* </Navbar.Section> */}
+                </Navbar>
+              }
+              /* aside={ */
+              /*   <MediaQuery smallerThan="sm" styles={{ display: 'none' }}> */
+              /*     <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}> */
+              /*       <Text>Application sidebar</Text> */
+              /*     </Aside> */
+              /*   </MediaQuery> */
+              /* } */
+              /* footer={ */
+              /*   <Footer height={60} p="md"> */
+              /*     Application footer */
+              /*   </Footer> */
+              /* } */
+              header={
+                <Header height={{ base: 50, md: 50 }} p="md">
+                  <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+                    <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
+                      <Burger
+                        opened={opened}
+                        onClick={() => setOpened((o) => !o)}
+                        size="sm"
+                        color={theme.colors.gray[6]}
+                        mr="xl"
+                      />
+                    </MediaQuery>
+
+                    <Text
+                      variant="gradient"
+                      gradient={{ from: 'indigo', to: 'cyan', deg: 45 }}
+                      sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
+                      ta="center"
+                      fz="xl"
+                      fw={700}
+                    >
+                      Health Management System
+                    </Text>
+                  </div>
+                </Header>
+              }
+            >
+              <Component {...pageProps} />
+            </AppShell>
+}
           </NotificationsProvider>
         </MantineProvider>
       </ColorSchemeProvider>
--- apps/hms/pages/dashboard/index.tsx
+++ apps/hms/pages/dashboard/index.tsx
@@ -1,87 +1,12 @@
-import { useState } from 'react';
-import {
-  AppShell,
-  Navbar,
-  Header,
-  Footer,
-  Aside,
-  Title,
-  Text,
-  MediaQuery,
-  Burger,
-  useMantineTheme,
-} from '@mantine/core';
-import useStyles from '../../styles';
-import { MainLinks, User } from '../../components';
+import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function AppShellDemo() {
-  const { classes } = useStyles();
-  const theme = useMantineTheme();
-  const [opened, setOpened] = useState(false);
+const Page = () => {
   return (
-    <AppShell
-      styles={{
-        main: {
-          background: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.gray[0],
-        },
-      }}
-      navbarOffsetBreakpoint="sm"
-      asideOffsetBreakpoint="sm"
-      navbar={
-        <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
-          <Navbar.Section grow mt="xs">
-            <MainLinks />
-          </Navbar.Section>
-          {/* <Navbar.Section> */}
-          {/*   <User /> */}
-          {/* </Navbar.Section> */}
-        </Navbar>
-      }
-      /* aside={ */
-      /*   <MediaQuery smallerThan="sm" styles={{ display: 'none' }}> */
-      /*     <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}> */
-      /*       <Text>Application sidebar</Text> */
-      /*     </Aside> */
-      /*   </MediaQuery> */
-      /* } */
-      /* footer={ */
-      /*   <Footer height={60} p="md"> */
-      /*     Application footer */
-      /*   </Footer> */
-      /* } */
-      header={
-        <Header height={{ base: 50, md: 50 }} p="md">
-          <div style={{ display: 'flex', alignItems: 'center', height: '100%' }}>
-            <MediaQuery largerThan="sm" styles={{ display: 'none' }}>
-              <Burger
-                opened={opened}
-                onClick={() => setOpened((o) => !o)}
-                size="sm"
-                color={theme.colors.gray[6]}
-                mr="xl"
-              />
-            </MediaQuery>
-
-            <Text
-              variant="gradient"
-              gradient={{ from: 'indigo', to: 'cyan', deg: 45 }}
-              sx={{ fontFamily: 'Greycliff CF, sans-serif' }}
-              ta="center"
-              fz="xl"
-              fw={700}
-            >
-              Health Management System
-            </Text>
-          </div>
-        </Header>
-      }
-    >
-      <Text>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</Text>
-      <Text>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</Text>
-      <Text>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</Text>
-      <Text>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</Text>
-      <Text>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</Text>
-      <Text>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat. Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.</Text>
-    </AppShell>
-  );
+    <div>Page</div>
+  )
 }
+
+Page.propTypes = {}
+
+export default Page
